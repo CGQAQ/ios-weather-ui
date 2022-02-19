05668ad42c0e4f818f72dba1b254f2134f688dff
CGQAQ <m.jason.liu@outlook.com>
2022-02-19 12:41:48 +0800

    init

---
+++ Weather UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather UI
+//
+//  Created by Jason Liu on 2022/2/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather UI/Weather_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_UIApp.swift
+//  Weather UI
+//
+//  Created by Jason Liu on 2022/2/19.
+//
+
+import SwiftUI
+
+@main
+struct Weather_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
